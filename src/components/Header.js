7a52124fb9dd4ffe9f78345a4435961cdb235846
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-09-28 02:36:30 +0300

    link components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header() {
+  return (
+    <header>
+      Hello World!
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
