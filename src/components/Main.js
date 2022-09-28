eb394e53c8a61539bc9258e6455a7feef4a24e91
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-09-28 03:13:11 +0300

    add body styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Main() {
-  return <main></main>;
+  return <main ></main>;
 }
 
 export default Main;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,9 @@
 import React from "react";
-import { ReactDOM } from "react";
-import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <div>
-      <NavLink to="/home" exact>
-        Home
-      </NavLink>
-
-      <NavLink to="/courses" exact>
-        Courses
-      </NavLink>
-
-      <NavLink to="/jobs" exact>
-        Jobs
-      </NavLink>
-
-      <NavLink to="/about" exact>
-        About
-      </NavLink>
-
-      <NavLink to="/contact_us" exact>
-        Contact Us
-      </NavLink>
+     
     </div>
   );
 }
