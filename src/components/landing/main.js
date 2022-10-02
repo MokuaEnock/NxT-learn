fe8a3e074f4a4c23633649520a89687ee89e2356
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-02 17:35:19 +0300

    create landing page skeleton

--- src/components/landing/main.js
+++ src/components/landing/main.js
@@ -4,19 +4,19 @@ function MainLanding() {
   return (
     <main>
       <h1 id="main_head1">
-        Discover self paced learning based on real world projects.
+        Discover self paced learning <br /> based on real world projects.
       </h1>
       <p id="main_par1">
         <span className="mainSpan"></span>
         <span className="mainSpan"></span>
         <span className="mainSpan"></span>
-        <span className="mainSpan"></span>
+        <button>Get Started</button>
       </p>
       <p id="main_par2">
-        <span className="mainSpan1">Self Paced</span>
-        <span className="mainSpan1">Project Paced</span>
-        <span className="mainSpan1">Experienced Instructors</span>
-        <span className="mainSpan1">Free Courses</span>
+        <span className="mainSpan1">Self paced</span>
+        <span className="mainSpan1">Project based</span>
+        <span className="mainSpan1">Experienced instructors</span>
+        <span className="mainSpan1">Free courses</span>
       </p>
     </main>
   );
