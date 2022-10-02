61bad90e9cc8a32715115fbca7109b331dd8339c
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-02 11:58:46 +0300

    remove redudant components in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
+    
     </>
   );
 }
