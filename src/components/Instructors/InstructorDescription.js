4ea5648e0b4017e20df6db456c8ab54ce5e1d2b9
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-03 16:33:12 +0300

    restyle instructor description

--- src/components/Instructors/InstructorDescription.js
+++ src/components/Instructors/InstructorDescription.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function InstructorDescription() {
-  return <div id="instructor_desc"></div>;
+  function Heading() {
+    return (
+      <div className="desc_inst">
+        <div className="desc_inst_img"></div>
+        <div className="desc_inst_info">
+          <h4>Enock Mokua</h4>
+          <p>Machine Learning Engineer</p>
+        </div>
+      </div>
+    );
+  }
+
+  function Details() {
+    return (
+      <p className="instructor_details">
+        A senior machine learning engineer at deloitte with 5 years of
+        experience and 7 years of instruction at University of Kent.
+      </p>
+    );
+  }
+  return (
+    <div id="instructor_desc">
+      <Heading />
+      <Details />
+    </div>
+  );
 }
 
 export default InstructorDescription;
