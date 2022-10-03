23daa61c1ef40ae9edd865ac4b0100780e646294
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-03 08:49:46 +0300

    create skeleton for description instruction

--- src/components/courses/Description.js
+++ src/components/courses/Description.js
@@ -3,10 +3,24 @@ import React from "react";
 function Description() {
   return (
     <div id="description">
-      <div className="desc_rating"></div>
-      <div className="desc_header"></div>
-      <div className="desc_inst"></div>
-      <div className="desc_body"></div>
+      <div className="desc_rating">
+        <span></span>
+        <span></span>
+      </div>
+      <h3 className="desc_header">Introduction To Machine Learning</h3>
+      <div className="desc_inst">
+        <div className="desc_inst_img"></div>
+        <div className="desc_inst_info">
+          <h4></h4>
+          <p></p>
+        </div>
+      </div>
+      <p className="desc_desc">
+        The NxT Learn complements the NxT project by providing skills for its
+        users the website implements this by providing courses based that
+        incoporate projects in its learning.
+      </p>
+      <ul className="desc_body"></ul>
       <div className="desc_footer"></div>
     </div>
   );
