62ba81cffeb4141c27f0ff2f2bccc43092410b5d
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-03 17:39:30 +0300

    add login form comp and connnect login compoent

---
+++ src/components/login/login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../landing/header";
+import Footer from "../landing/footer";
+import LoginForm from "./LoginForm";
+
+function Login() {
+  return (
+    <>
+      <Header />
+      <LoginForm />
+      <Footer />
+    </>
+  );
+}
+
+export default Login;
