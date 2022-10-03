25619a4319ab1f823c73138001a77e0113f84ec8
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-03 18:05:24 +0300

    Create home component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import About from "./components/about/about";
 import Landing from "./components/landing/landing";
 import Courses from "./components/courses/courses";
 import Instructor from "./components/Instructors/Instructors";
-
+import Login from "./components/login/login";
+import Home from "./components/Home/Home.js";
 function App() {
   return (
     <>
       <Landing />
+      <Home />
       <Courses />
       <Instructor />
+      <Login />
       <About />
     </>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Aside from "../courses/Aside";
+
+function Home() {
+  return (
+    <section id="home">
+      <Aside />
+    </section>
+  );
+}
+
+export default Home;
