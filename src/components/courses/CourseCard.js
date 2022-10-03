9bddba07d071ffbf67a7a4cc7823a37db181856e
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-03 06:21:02 +0300

    create job card skeleton

--- src/components/courses/CourseCard.js
+++ src/components/courses/CourseCard.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 function Card() {
-  return <li>
-    
-  </li>;
+  return (
+    <li className="job_card">
+      <div className="jobCard_header"></div>
+      <div className="jobCard_body">
+        <div className="jobCard_title">UI Vektor illustration Design</div>
+        <div className="jobCard_spans">
+          <span className="jobCard_span"></span>
+          <span className="jobCard_span"></span>
+          <span className="jobCard_span"></span>
+        </div>
+      </div>
+      <button className="jobCard_footer">View More</button>
+    </li>
+  );
 }
 
 export default Card;
