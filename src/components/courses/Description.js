6805ee0b9aad07f72e84a62552060ce8fdc78d91
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-03 08:06:29 +0300

    create description skeleton

--- src/components/courses/Description.js
+++ src/components/courses/Description.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Description() {
-  return <div id="description"></div>;
+  return (
+    <div id="description">
+      <div className="desc_rating"></div>
+      <div className="desc_header"></div>
+      <div className="desc_inst"></div>
+      <div className="desc_body"></div>
+      <div className="desc_footer"></div>
+    </div>
+  );
 }
 
 export default Description;
