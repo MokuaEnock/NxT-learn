34ee237ecce7f5cda90609c0e6e1c3ab5f61d500
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-04 05:52:42 +0300

    create login form

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function LoginForm() {
-  return <form id="loginform">
-    <div></div>
-  </form>;
+  return (
+    <form id="loginform">
+      <div>
+        <label for="username">User Name</label>
+        <input type="text" id="username" />
+        <label for="email">Enter Email</label>
+        <input type="email" id="email" />
+        <label for="password1">Set password</label>
+        <input type="password" id="password1" placeholder="8+ Characters" />
+        <label for="password2">Repeat password</label>
+        <input type="password" id="password2" />
+        <button>Create Account</button>
+      </div>
+    </form>
+  );
 }
 
 export default LoginForm;
