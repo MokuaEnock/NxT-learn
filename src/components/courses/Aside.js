5c5dc8fb5eb729d082a020ea17353e67af10d49f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-04 06:11:08 +0300

    create skeleton of aside bar

--- src/components/courses/Aside.js
+++ src/components/courses/Aside.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 function Aside() {
-  return <aside>
-    
-  </aside>;
+  return (
+    <aside>
+      <a>
+        <img src=".../assets/" alt="logo" />
+      </a>
+      <nav>
+        <a>Home</a>
+        <a>Courses</a>
+        <a>Instructors</a>
+        <a>Your Path</a>
+        <a>Logout</a>
+      </nav>
+    </aside>
+  );
 }
 
 export default Aside;
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Header from "../landing/header";
-import Footer from "../landing/footer";
 import LoginForm from "./LoginForm";
 
 function Login() {
