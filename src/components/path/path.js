06c49209432420967e3b0bf51e59148acba3467f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-06 00:08:53 +0300

    add courseview to path component

--- src/components/path/path.js
+++ src/components/path/path.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Aside from "../courses/Aside";
 import PathList from "./pathlist";
+import CourseView from "./courseview";
 
 function Path() {
   return (
     <>
       <Aside />
       <PathList />
+      <CourseView />
     </>
   );
 }
