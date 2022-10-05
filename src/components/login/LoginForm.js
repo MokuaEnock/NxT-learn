15f132a16ebf87d6dc0ee19bd3902f3aab58886e
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-05 05:07:27 +0300

    restyle form

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -4,27 +4,46 @@ function LoginForm() {
   return (
     <form id="loginform">
       <div id="logform">
-        <label htmlFor="username">User Name</label>
-        <input type="text" id="username" required />
-        <label htmlFor="email">Enter Email</label>
-        <input type="email" id="emaill" required />
-        <label htmlFor="password1">Set password</label>
-        <input
-          type="password"
-          id="password1"
-          placeholder="8+ Characters"
-          required
-          autoComplete="_blank"
-        />
-        <label htmlFor="password2">Repeat password</label>
-        <input
-          type="password"
-          id="password2"
-          required
-          autoComplete="_blank"
-          placeholder="Reapeat password"
-        />
-        <button>Create Account</button>
+        <span>
+          <span>
+            <h2>Welcome to NxT learn</h2>
+            <p>Create your Account</p>
+          </span>
+
+          <span>
+            <label htmlFor="username">User Name</label>
+            <input type="text" id="username" required />
+          </span>
+
+          <span>
+            <label htmlFor="email">Enter Email</label>
+            <input type="email" id="emaill" required />
+          </span>
+
+          <span>
+            <label htmlFor="password1">Set password</label>
+            <input
+              type="password"
+              id="password1"
+              placeholder="8+ Characters"
+              required
+              autoComplete="_blank"
+            />
+          </span>
+
+          <span>
+            <label htmlFor="password2">Repeat password</label>
+            <input
+              type="password"
+              id="password2"
+              required
+              autoComplete="_blank"
+              placeholder="Repeat password"
+            />
+          </span>
+
+          <button>Create Account</button>
+        </span>
       </div>
       <div id="imagform"></div>
     </form>
