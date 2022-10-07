b629e3c77a1c6e7a081696f8e3f2dd3ebc476ea0
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-07 09:43:59 +0300

    add social images

--- src/components/landing/footer.js
+++ src/components/landing/footer.js
@@ -3,7 +3,14 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      
+      <div id="footer_cont1">
+      </div>
+      <div id="footer_cont2">
+        <a href="">
+        <img src="../"/>
+        </a>
+      </div>
+      <div id="footer_cont3"></div>
     </footer>
   );
 }
