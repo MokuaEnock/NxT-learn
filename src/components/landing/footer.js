ed386c91803a705a145b169ccda734ea82e688b7
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-07 10:11:49 +0300

    add socials link

--- src/components/landing/footer.js
+++ src/components/landing/footer.js
@@ -1,13 +1,33 @@
 import React from "react";
+import twitter from "../assets/twitter.png";
+import linkedin from "../assets/linkedin.png";
+import instagram from "../assets/instagram.png";
+import github from "../assets/github-logo.png";
 
 function Footer() {
   return (
     <footer>
-      <div id="footer_cont1">
-      </div>
+      <div id="footer_cont1"></div>
+
       <div id="footer_cont2">
-        <a href="">
-        <img src="../"/>
+        <a href="https://github.com/MokuaEnock">
+          <img
+            src={github}
+            alt="github"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/enock-mokua-b7a37224b/">
+          <img
+            src={linkedin}
+            alt="linkedin"
+          />
+        </a>
+        <a href="https://twitter.com/254_enok">
+          <img src={twitter} alt="twitter" />
+        </a>
+
+        <a href="https://www.instagram.com/enok_254/">
+          <img src={instagram} alt="instagram" />
         </a>
       </div>
       <div id="footer_cont3"></div>
