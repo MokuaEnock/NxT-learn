4f33f9dbbc8f363c35c1a2a3e6468175d7fcc399
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-08 11:27:50 +0300

    delete redudant code and reconfigure

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import "./App.css";
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./components/about/about";
-import Login from "./components/login/login";
-import Contact from "./components/contact/contact";
+import "./App.js";
+import Home from "./components/Home.js";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Home />
+    </>
   );
 }
 
 export default App;
-
-/* <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/instructor" element={<Instructor />} />
-        <Route path="/path" element={<Path />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter> */
