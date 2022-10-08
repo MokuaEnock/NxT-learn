b99b886898c220b5034ee1b3a8d42a8c49573770
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-08 20:26:09 +0300

    create skeleton for login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Login() {
-  return <form id="login">Login</form>;
+  return (
+    <form id="login">
+      <section>
+        <div id="get_started">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div id="log_in">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </section>
+    </form>
+  );
 }
 
 export default Login;
