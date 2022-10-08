ca7514a34cac05ad3ed73be4dac1bfc1ed591749
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-08 20:05:17 +0300

    add login and get started section

--- src/components/home/main.js
+++ src/components/home/main.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Main() {
-  return <main>Main</main>;
+  return (
+    <main>
+      Main
+      <button>Get Started</button>
+      <button>Continue</button>
+    </main>
+  );
 }
 
 export default Main;
