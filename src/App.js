d722b71db6247db0b88d470d3327ce0a8194bb32
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-08 11:14:06 +0300

    delete unneeded code

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/about/about";
-import Landing from "./components/landing/landing";
 import Login from "./components/login/login";
 import Contact from "./components/contact/contact";
-import Home from "./components/Home/Home";
-import Instructor from "./components/Instructors/Instructors";
-import Courses from "./components/courses/courses";
-import Path from "./components/path/path";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/instructor" element={<Instructor />} />
-        <Route path="/path" element={<Path />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
--- src/components/path/courseview.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CourseView() {
-  return <section id="pathview"></section>;
-}
-
-export default CourseView;
