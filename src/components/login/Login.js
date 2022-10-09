1a691a53e42b65ca92ff496cfaa73ef573d74a96
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-09 21:32:23 +0300

    create final state to get started inputs

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { json } from "server/reply";
 
 function Login() {
   let [start, setStart] = useState({
@@ -11,7 +12,7 @@ function Login() {
   });
 
   function submit() {
-    console.log(start);
+    console.log(JSON.stringify(start));
   }
 
   return (
@@ -24,11 +25,17 @@ function Login() {
               type="text"
               placeholder="First Name"
               value={start.first_name}
+              onChange={(e) =>
+                setStart({ ...start, first_name: e.target.value })
+              }
             />
             <input
               type="text"
               placeholder="Last Name"
               value={start.last_name}
+              onChange={(e) =>
+                setStart({ ...start, last_name: e.target.value })
+              }
             />
           </span>
 
@@ -37,20 +44,31 @@ function Login() {
               type="email"
               placeholder="Your Email"
               value={start.email}
-              onChange={(e) => setStart({ email: e.target.value })}
+              onChange={(e) => setStart({ ...start, email: e.target.value })}
             />
-            <input type="text" placeholder="Your GitHub" value={start.github} />
+            <input
+              type="text"
+              placeholder="Your GitHub"
+              value={start.github}
+              onChange={(e) => setStart({ ...start, github: e.target.value })}
+            />
           </span>
           <span>
             <input
               type="password"
               placeholder="Enter password"
               value={start.password1}
+              onChange={(e) =>
+                setStart({ ...start, password1: e.target.value })
+              }
             />
             <input
               type="password"
               placeholder="Re-enter password"
               value={start.password2}
+              onChange={(e) =>
+                setStart({ ...start, password2: e.target.value })
+              }
             />
           </span>
           <span>
