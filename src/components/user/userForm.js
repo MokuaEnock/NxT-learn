c84eea7ee45547810b2b33755410fb5429a57184
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-11 00:00:50 +0300

    craete use state

--- src/components/user/userForm.js
+++ src/components/user/userForm.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UserForm() {
+  let [form, setForm] = useState({
+    user_path: "",
+    user_programing_level: "",
+    user_job_experience: "",
+    user_job_interest: "",
+    user_career_objectives: "",
+  });
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("hello World")
+    console.log("hello World");
+    console.log(form);
   }
   return (
     <section id="userForm">
@@ -12,8 +20,14 @@ function UserForm() {
         <form id="userForm1"></form>
         <form id="userForm2" onSubmit={handleSubmit}>
           <span>
-            <label htmlFor="path_type">Which path would you like to follow?</label>
-            <select name="path_type" id="path_type">
+            <label htmlFor="path_type">
+              Which path would you like to follow?
+            </label>
+            <select
+              name="path_type"
+              id="path_type"
+              onChange={(e) => setForm({ ...form, user_path: e.target.value })}
+            >
               <option>Select Path</option>
               <option value="self_led">Self Led</option>
               <option value="instructor_led">Instructor Led</option>
@@ -21,8 +35,16 @@ function UserForm() {
             </select>
           </span>
           <span>
-            <label htmlFor="skill_level">What is your level in programming?</label>
-            <select name="skill_levels" id="skill_level">
+            <label htmlFor="skill_level">
+              What is your level in programming?
+            </label>
+            <select
+              name="skill_levels"
+              id="skill_level"
+              onChange={(e) =>
+                setForm({ ...form, user_programming_level: e.target.value })
+              }
+            >
               <option>Select Experience Level</option>
               <option value="novice">Novice</option>
               <option value="a_beginner">Advanced Beginner</option>
@@ -32,8 +54,16 @@ function UserForm() {
             </select>
           </span>
           <span>
-            <label htmlFor="job_experience">Do you have any job experience</label>
-            <select name="job_experience" id="job_experience">
+            <label htmlFor="job_experience">
+              Do you have any job experience
+            </label>
+            <select
+              name="job_experience"
+              id="job_experience"
+              onChange={(e) =>
+                setForm({ ...form, user_job_experience: e.target.value })
+              }
+            >
               <option>Select Experience Level</option>
               <option value="novice">Novice</option>
               <option value="a_beginner">I dont have any</option>
@@ -47,7 +77,13 @@ function UserForm() {
               Are you looking for new opportunities after completing some
               courses?
             </label>
-            <select name="work_experience" id="work_experience">
+            <select
+              name="work_experience"
+              id="work_experience"
+              onChange={(e) =>
+                setForm({ ...form, user_job_interest: e.target.value })
+              }
+            >
               <option>Have you had any experience?</option>
               <option value="yes_exp">Yes</option>
               <option value="no_exp">No</option>
@@ -56,7 +92,15 @@ function UserForm() {
 
           <span>
             <label>Please provide your career objectives</label>
-            <textarea id="career_objectives" rows="5" columns="33" />
+            <textarea
+              id="career_objectives"
+              rows="5"
+              value={form.first_name}
+              columns="33"
+              onChange={(e) =>
+                setForm({ ...form, user_career_objectives: e.target.value })
+              }
+            />
           </span>
 
           <span>
