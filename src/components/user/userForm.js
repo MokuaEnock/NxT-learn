fca56ac03e12a18c07d36fe809a1fef0f8829e03
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-10 20:53:01 +0300

    add span to forms

--- src/components/user/userForm.js
+++ src/components/user/userForm.js
@@ -6,7 +6,29 @@ function UserForm() {
       <p>Hello there Enock</p>
       <div>
         <form id="userForm1"></form>
-        <form id="userForm2"></form>
+        <form id="userForm2">
+          <span>
+            <label form="path_type">Which path would you like to follow?</label>
+            <select name="path_type" id="path_type">
+              <option>Select Path</option>
+              <option value="self_led">Self Led</option>
+              <option value="instructor_led">Instructor Led</option>
+              <option value="hybrid">Hybrid</option>
+            </select>
+          </span>{" "}
+          <span>
+            <label></label>
+          </span>{" "}
+          <span>
+            <label></label>
+          </span>{" "}
+          <span>
+            <label></label>
+          </span>{" "}
+          <span>
+            <label></label>
+          </span>
+        </form>
       </div>
     </section>
   );
