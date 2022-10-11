884523b8b93aedfb79b68abf9f15101467c130b7
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-11 06:55:32 +0300

    create skeleton for user home component

--- src/components/user/userHome.js
+++ src/components/user/userHome.js
@@ -5,7 +5,30 @@ function UserHome() {
   return (
     <>
       <UserAside />
-      <section id="userHome">User Home component</section>
+      <section id="userHome">
+        <h2>Discover Courses.</h2>
+        <form>
+          <input type="text" placeholder="Search Courses" />
+
+          <select placeholder="Select Language">
+            <option>Select Language</option>
+            <option value="python">Python</option>
+            <option value="javascript">Javascript</option>
+            <option value="html_css">HTML & CSS</option>
+          </select>
+
+          <select>
+            <option>Select Framework or Module</option>
+            <option value="pandas">Pandas</option>
+            <option value="react">React</option>
+            <option value="bootstrap">Bootstrap</option>
+            <option value="svelte">Svelte</option>
+          </select>
+        </form>
+        <div>
+          
+        </div>
+      </section>
       <UserFooter />
     </>
   );
