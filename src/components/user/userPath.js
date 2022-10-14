b645aa3dc4d459f8075ea6d63e5e012aaf575662
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-14 20:52:32 +0300

    create skeleton for

--- src/components/user/userPath.js
+++ src/components/user/userPath.js
@@ -3,11 +3,20 @@ import UserAside from "./userAside";
 import UserFooter from "./userFooter";
 
 function UserPath() {
+  function ListItem() {
+    return <li id="course_card"></li>;
+  }
   return (
     <>
       <UserAside />
       <section id="user_path">
-        user Path
+        <ul id="path_list">
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </ul>
+        <ul id="path_description"></ul>
       </section>
       <UserFooter />
     </>
