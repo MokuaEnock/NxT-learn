19f62de81d08e23119fd59d28043c5c36671befb
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-14 16:44:16 +0300

    connect user path to userhome component

---
+++ src/components/user/userPath.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserPath() {
+  return <></>;
+}
+
+export default UserPath;
--- src/components/user/userView.js
+++ src/components/user/userView.js
@@ -3,12 +3,14 @@ import UserForm from "./userForm";
 import UserAside from "./userAside";
 import UserFooter from "./userFooter";
 import UserHome from "./userHome";
+import UserPath from "./userPath";
 function UserView() {
   return (
     <>
       <UserAside />
       <UserForm />
       <UserHome />
+      <UserPath />
       <UserFooter />
     </>
   );
