b5df476de7b4bb857d158430eea1f6cc37f65d51
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-16 00:01:55 +0300

    add contente to landing page

--- src/components/home/main.js
+++ src/components/home/main.js
@@ -2,11 +2,23 @@ import React from "react";
 
 function Main() {
   return (
-    <main>
-      Main
-      <button>Get Started</button>
-      <button>Continue</button>
-    </main>
+    <>
+      <main>
+        <h2>Teaching is more than lecturing</h2>
+        <div>
+          <h3>We can upgrade your skills a full 360*</h3>
+          <p>Learn how we can help</p>
+        </div>
+        <div>
+          <p>Teaching and learning re-engineered</p>
+          <button>Explore</button>
+        </div>
+        <button>Get Started</button>
+        <button>Continue</button>
+      </main>
+
+      <section id="main_section"></section>
+    </>
   );
 }
 
