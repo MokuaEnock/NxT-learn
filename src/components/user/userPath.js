504d994aee906bcea62a6493531147b50b0d7d88
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-18 23:39:32 +0300

    restyle cart cards

--- src/components/user/userPath.js
+++ src/components/user/userPath.js
@@ -4,13 +4,16 @@ import UserFooter from "./userFooter";
 
 function UserPath() {
   function ListItem() {
-    return <li id="course_card">
-         <h2>Hello</h2>
-        <span>
-          <button>Remove</button>
-          <button>Start</button>
+    return (
+      <li id="course_card"   >
+        <h2 id="course_card_title">Title</h2>
+        <ul id="course_card_instructors">instructors</ul>
+        <span id="course_card_buttons">
+          <button>Add to Path</button>
+          <button>View</button>
         </span>
-    </li>;
+      </li>
+    );
   }
   return (
     <>
