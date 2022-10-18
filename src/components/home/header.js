d4819d837a732f2b22051e810cde41156f99fd9e
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-19 00:32:09 +0300

    create contene skeleton for landing page

--- src/components/home/header.js
+++ src/components/home/header.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Header() {
-  return <header id="header">Header</header>;
+  return (
+    <header id="header">
+      <p>Instructors</p>
+      <p>Courses</p>
+      <h2>NxT Learn</h2>
+      <p>Contact Us</p>
+      <p>About Us</p>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/home/main.js
+++ src/components/home/main.js
@@ -4,20 +4,30 @@ function Main() {
   return (
     <>
       <main>
-        <h2>Teaching is more than lecturing</h2>
-        <div>
-          <h3>We can upgrade your skills a full 360*</h3>
-          <p>Learn how we can help</p>
+        <div id="container1">
+          <span></span>
+          <span>Develop Tech skills form the best source.</span>
+          <span></span>
+          <span>
+            <button>Get Started</button>
+            <button>Continue</button>
+          </span>
         </div>
-        <div>
-          <p>Teaching and learning re-engineered</p>
-          <button>Explore</button>
-        </div>
-        <button>Get Started</button>
-        <button>Continue</button>
+        <div id="container2"></div>
       </main>
 
-      <section id="main_section"></section>
+      <section id="main_section">
+        <div id="sect_container1">
+          <h2>
+            From writing your first Hello World to creating a neural network
+          </h2>
+
+          <h2>Fully committed to equiping learners with real World skills.</h2>
+        </div>
+        <ul id="sect_container2">
+          <li></li>
+        </ul>
+      </section>
     </>
   );
 }
