af9dabd7a1459a019143e99afff40e4b3db43f37
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-19 22:00:36 +0300

    create skeleton for section 1 in landing page

--- src/components/home/main.js
+++ src/components/home/main.js
@@ -18,15 +18,27 @@ function Main() {
 
       <section id="main_section">
         <div id="sect_container1">
-          <h2>
-            From writing your first Hello World to creating a neural network
-          </h2>
+          <div>
+            <h2>From writing your first</h2>
+            <code> Hello World </code>
+            <h2>to creating a neural network.</h2>
+          </div>
 
-          <h2>Fully committed to equiping learners with real World skills.</h2>
+          <div>
+            <h2>
+              Fully committed to equiping learners with real World skills.
+            </h2>
+          </div>
         </div>
         <ul id="sect_container2">
           <li></li>
+          <li></li>
+          <li></li>
         </ul>
+      </section>
+
+      <section id="main_cards">
+        
       </section>
     </>
   );
