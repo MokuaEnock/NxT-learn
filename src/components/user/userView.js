b9259f5f5881720862e1051f69ad14324b6d924f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-21 15:03:20 +0300

    fetch github data

--- src/components/user/userView.js
+++ src/components/user/userView.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserForm from "./userForm";
 import UserAside from "./userAside";
 import UserFooter from "./userFooter";
 import UserHome from "./userHome";
 import UserPath from "./userPath";
 function UserView() {
-  
+  let [item, setItem] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/courses")
+      .then((r) => r.json())
+      .then((r) => setItem(r))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(item);
+
   return (
     <>
       <UserAside />
