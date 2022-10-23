6bec4836801b5e3e7bfd68b8bb7b984039796a10
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-23 13:26:30 +0300

    style course card and reassign card

--- src/components/user/userHome.js
+++ src/components/user/userHome.js
@@ -1,29 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import UserAside from "./userAside";
 import UserFooter from "./userFooter";
+
 function UserHome({ courses }) {
   let [view, setView] = useState([]);
   let [cart, setCart] = useState([]);
-  /* let itemList = item.map((e, index) => {
-    let instructors = e.visible_instructors;
-    let insts = instructors.map((i, index) => {
-      return (
-        <a href={i.url} key={index} id="course_card_instructor_items">
-          <img src={i.image_100x100} alt="instructor" />
-        </a>
-      );
-    });
-    return (
-      <li id="course_card" key={index}>
-        <h2 id="course_card_title">{e.title}</h2>
-        <ul id="course_card_instructors">{insts}</ul>
-        <span id="course_card_buttons">
-          <button onClick={addnew(index)}>Add to Path</button>
-          <button onClick={handler(index)}>View</button>
-        </span>
-      </li>
-    );
-  }); */
+
+  console.log(courses);
 
   /* create function to handleview button */
   let handleView = (index) => () => {
@@ -32,7 +15,6 @@ function UserHome({ courses }) {
   };
 
   /* function to add handle add to cart */
-
   let handlePath = (index) => () => {
     let newItem = courses[index];
     setCart([...cart, newItem]);
@@ -41,10 +23,15 @@ function UserHome({ courses }) {
   /* add items to the dom */
   let items = courses.map((i, index) => {
     return (
-      <li id="course_card" key={i.course_id}>
-        <h2 id="course_card_title">{i.course_title}</h2>
-        <ul id="course_card_instructors"></ul>
-        <span id="course_card_buttons">
+      <li className="course_card" key={i.course_id}>
+        <span className="course_card_image"></span>
+        <span className="course_card_rating">
+          <p>{i.rating}</p>
+          <p> {i.course_hours}</p>
+        </span>
+        <span className="course_card_title">{i.course_title}</span>
+        <span className="course_card_detail">{i.course_description}</span>
+        <span className="course_card_buttons">
           <button onClick={handlePath(index)}>Add to Path</button>
           <button onClick={handleView(index)}>View</button>
         </span>
@@ -54,7 +41,6 @@ function UserHome({ courses }) {
 
   /* create a unique list of cart items */
   const uniqueCart = [...new Set(cart)];
-
   let itemDescriptor = view.map((e) => {
     return (
       <li key={e.course_id}>
@@ -63,6 +49,7 @@ function UserHome({ courses }) {
     );
   });
 
+  /* log cart items */
   let cartItems = uniqueCart.map((e, index) => {
     return (
       <li id="cart_card" key={index}>
@@ -78,6 +65,7 @@ function UserHome({ courses }) {
     );
   });
 
+  /* ca */
   function displayCart(e) {
     e.preventDefault();
     let cart = document.querySelector("#items_cart");
